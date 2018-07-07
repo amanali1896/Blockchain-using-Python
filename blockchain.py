bf176f67db63aabdec3b3ec957733e64b34f31e3
Aman Ali <pamanalionline@gmail.com>
2018-07-07 19:20:23 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  7 19:09:31 2018
 
+@author: Aman Ali Pogaku
+"""
+
+# Blockchain using Python
+# Install Flask version 0.12.2 by using pip 
+# Install Postman HTTP client: https://www.getpostman.com/
