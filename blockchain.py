8b3e92bdb7104e7e4e2ae163ae0e3f451cbb789a
Aman Ali <pamanalionline@gmail.com>
2018-07-07 19:40:39 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -6,5 +6,12 @@ Created on Sat Jul  7 19:09:31 2018
 """
 
 # Blockchain using Python
-# Install Flask version 0.12.2 by using pip 
-# Install Postman HTTP client: https://www.getpostman.com/
+# Install Flask version 0.12.2 by using pip install Flask==0.12.2
+# Install Postman HTTP client: https://www.getpostman.com/ 
+
+#Importing libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
