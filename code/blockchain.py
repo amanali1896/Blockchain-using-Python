90df0640b67acf8eaadc31b340d597ff4b09e662
Aman Ali <pamanalionline@gmail.com>
2018-07-07 19:57:34 -0400

    Create blockchain.py

---
+++ code/blockchain.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  7 19:09:31 2018
+
+@author: Aman Ali Pogaku
+"""
+
+# Blockchain using Python
+# Install Flask version 0.12.2 by using pip install Flask==0.12.2
+# Install Postman HTTP client: https://www.getpostman.com/ 
+
+#Importing libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify 
+
